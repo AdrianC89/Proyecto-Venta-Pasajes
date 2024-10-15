408462f38e8ab782e11156ab1e53b4e70fa1d877
Adrián <adriancoroneltandil@gmail.com>
2024-10-15 20:02:08 -0300

    formulario de creacion hecho

--- React-Pasajes/venta-pasajes/src/api/pasajes.js
+++ React-Pasajes/venta-pasajes/src/api/pasajes.js
@@ -5,3 +5,11 @@ const API_URL = 'http://localhost:3000/api/pasajes'; // Cambia esto por la URL d
 export const getPasajes = async () => {
     return await axios.get(API_URL);
 };
+
+export const createPasaje = async (pasajeData) => {
+    return await axios.post(API_URL, pasajeData, {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        },
+    });
+};
