b9943b546544c5ada5e6be24551b1d78318cb13a
VCIFARELLI <virginiacifarelli2018@gmail.com>
2024-10-15 14:51:56 -0300

    rutas en app.jsx

--- React-Pasajes/venta-pasajes/src/App.jsx
+++ React-Pasajes/venta-pasajes/src/App.jsx
@@ -4,7 +4,10 @@ import { AuthProvider } from './context/AuthContext'
 import RegisterPage from './pages/RegisterPage'
 import LoginPage from './pages/LoginPage'
 import HomePage from './pages/HomePage'
-
+import ListaPasajes from "./pages/ListaPasajes";
+import CrearPasajes from "./pages/CrearPasajes";
+import ActualizarPasajes from "./pages/ActualizarPasajes";
+import Perfil from "./pages/Perfil";
 
 function App() {
   return (
@@ -15,10 +18,10 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/pasajes" element={<h1>Lista de Pasajes</h1>} />
-        <Route path="/crearpasaje" element={<h1>Crear Pasajes</h1>} />
-        <Route path="/pasajes/:id" element={<h1>Actualizar Pasaje</h1>} />
-        <Route path="/profile" element={<h1>Perfil</h1>} />
+        <Route path="/pasajes" element={<ListaPasajes />} />
+        <Route path="/crearpasaje" element={<CrearPasajes/>} />
+        <Route path="/pasajes/:id" element={<ActualizarPasajes />} />
+        <Route path="/profile" element={<Perfil />} />
 
       </Routes>
     </BrowserRouter>
---
+++ React-Pasajes/venta-pasajes/src/pages/ActualizarPasajes.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ActualizarPasajes() {
+  return (
+    <div>ActualizarPasajes</div>
+  )
+}
