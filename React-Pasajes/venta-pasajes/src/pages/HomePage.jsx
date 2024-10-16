ae0c0461e7310c1403a094497f43b07e05ec0daf
Melissa Ferrari <melissa.ferrari.v@gmail.com>
2024-10-16 07:16:29 -0300

    se trabajo sobre la vista del home

--- React-Pasajes/venta-pasajes/src/pages/HomePage.jsx
+++ React-Pasajes/venta-pasajes/src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PromocionCuotas from '../components/PromocionCuotas';
+import AboutTandilBus from '../components/AboutTandilBus';
 
 
 
@@ -7,10 +9,18 @@ const HomePage = () => {
   return (
     <div className="flex flex-col min-h-screen">
 
-      
+
       {/* Contenido Principal */}
       <main className="flex-grow">
         <h1>Bienvenido a la Página Principal</h1>
+        <div className="mb-10">
+        <PromocionCuotas />
+        </div>
+        
+        <div className="mb-10">
+        <AboutTandilBus />
+        </div>
+        
       </main>
       
 
